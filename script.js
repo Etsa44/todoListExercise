28c1a783213f852e28e1cdc2d7ce2025f310d619
etsa kambandji <kmjetsa@gmail.com>
2025-07-03 00:59:55 +0200

    i finish to get a user value

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
+// j'appelle mon bouton depuis mon html
 const button = document.getElementById("myButton")
-
+const tbody = document.getElementById("tbody")
+// fonction pour recuperer les donnees 
 function getValue(){
+    // ces deux ligne permettent de rendre mon tabeau visible
+    const table = document.getElementById("tableTask")
+    table.style.display = "block"
+    // cette ligne supprime le comportement par defaut du bouton qui est de reactualiser la page lors du clique
+    event.preventDefault();
+    // je recupere la valeur soumis a mon input title
     const getTitle = document.getElementById("taskTitle").value
+    // je recupere la valeur soumis a mon input description
     const getDescription = document.getElementById("taskDescription").value
-    console.log(getTitle,getDescription)
+    // je recupere la valeur de mon select 
+    const getPriority = document.getElementById("taskPriority").value
+    // creation de container pour afficher les donnees et insertion des donnees
+    const tr = document.createElement("tr")
+    const titleContenair = document.createElement("td")
+    titleContenair.textContent = getTitle
+    const descriptionContenair = document.createElement("td")
+    descriptionContenair.textContent = getDescription
+    const priorityContenair = document.createElement("td")
+    priorityContenair.textContent = getPriority
+    // imbrication de contenaire
+    tr.appendChild(titleContenair)
+    tr.appendChild(descriptionContenair)
+    tr.appendChild(priorityContenair)
+    tbody.appendChild(tr)
+    // les 3 lignes qui suivent c'est pour reactualiser les champs remplis apres soumission 
+    document.getElementById("taskTitle").value = "";
+    document.getElementById("taskDescription").value = "";
+    document.getElementById("taskPriority").value = "";
+    // j'affiche tout a la console pour verifiee
+    console.log(getTitle,getDescription,getPriority)
 }
-
+// j'ajoute un ecouteur d'evenement au click de mon bouton qui est l'execution de ma fonction
 button.addEventListener("click",getValue)
